79217a978602fed18e3697973e78c35737ddcdc6
Caio <Belfort>
2018-08-17 17:01:10 -0300

    Execution with parallel hint working but only with the threading backend

--- parallel_queries.py
+++ parallel_queries.py
@@ -1,6 +1,7 @@
 import typing
 import re
 import sqlalchemy
+from joblib import Parallel, delayed
 
 
 def get_named_params(stmt: str) -> typing.List[str]:
@@ -36,7 +37,8 @@ def get_parallel_hinted_params(parameters: typing.List[str]) -> typing.List[str]
 
 def execute_query_in_parallel(connectable: sqlalchemy.engine.Connectable,
                               stmt: str,
-                              parameters: dict
+                              parameters: dict,
+                              n_jobs: int = 4,
                               ) -> list:
     """
     Executes a query in parallel, choosing the best splitter parameter
@@ -44,9 +46,14 @@ def execute_query_in_parallel(connectable: sqlalchemy.engine.Connectable,
         connectable: A connectable object from sqlalchemy.
         stmt: The SQL statement, if the parallels hint are.
         parameters: A dictionary of parameters defined in the SQL statement.
+        n_jobs: The number of workers to use.
     Returns:
         A list containing the rows retrived.
     """
+
+    def run_query(param):
+        """Closure to run the query inside delayed"""
+        return connectable.execute(sqlalchemy.text(stmt), param).fetchall()
 
     # Check if the *parameters* containing all values required by the query
     required_parameters = get_named_params(stmt)
@@ -64,5 +71,22 @@ def execute_query_in_parallel(connectable: sqlalchemy.engine.Connectable,
     if len(parallel_parameters) == 0:
         return connectable.execute(sqlalchemy.text(stmt), parameters).fetchall()
 
+    # Get values from parallel parameters keeping only list values
+    parallel_param_values = {k: parameters[k] for k in parallel_parameters if isinstance(parameters[k], list)}
+
     # Choose the best for split the statement
-    return []
+    lengths = {k: len(parallel_param_values[k]) for k in parallel_param_values}
+
+    # This choose the best param based on the list len
+    best_param = max(lengths.keys(), key=lambda key: lengths[key])
+
+    best_param_values = parameters[best_param]
+    del parameters[best_param]  # remove best param from the dict
+
+    with Parallel(n_jobs=n_jobs, backend='threading') as pwork:
+        result = pwork(
+            delayed(run_query)(dict(parameters, **{best_param: v}))
+            for v in best_param_values
+        )
+
+    return result
--- test.py
+++ test.py
@@ -1,8 +1,17 @@
 import unittest
 import parallel_queries as pqueries
 import sqlalchemy as sa
+import os
 
 class Tests(unittest.TestCase):
+    def setUp(self):
+        self.eng = sa.create_engine('sqlite:///test.db')
+        self.eng.execute('CREATE TABLE t (val1 INT, val2 INT)')
+        self.eng.execute('INSERT INTO t VALUES (1, 1), (2, 3), (5, 4), (8, 8)')
+
+    def tearDown(self):
+        os.remove('test.db')
+
 
     def test_correct_named_param_from_query(self):
         query = """
@@ -16,7 +25,7 @@ class Tests(unittest.TestCase):
         computed_named_params = pqueries.get_named_params(query)
         expected_named_params = ['param_id', 'param_value']
 
-        self.assertEquals(set(computed_named_params), set(expected_named_params), 'Named params not equal expected.')
+        self.assertEqual(set(computed_named_params), set(expected_named_params), 'Named params not equal expected.')
 
     def test_empty_named_params_from_query(self):
         query = "SELECT TOP 10 * FROM table WHERE x = 2"
@@ -30,7 +39,6 @@ class Tests(unittest.TestCase):
         parallel_params = pqueries.get_parallel_hinted_params(params)
 
         self.assertEqual(set(parallel_params), {'param_x_PARALLEL', 'param_z_PARALLEL'})
-
 
     def test_execute_query_in_parallel_raises_parameter_missing(self):
         """Tests if all parameters are defined for the query"""
@@ -49,29 +57,36 @@ class Tests(unittest.TestCase):
 
     def test_execute_query_in_parallel_run_without_hint(self):
 
-
-        eng = sa.create_engine('sqlite:///:memory:')
-
-        eng.execute('CREATE TABLE t (val1 INT, val2 INT)')
-        eng.execute('INSERT INTO t VALUES (1, 1), (2, 3), (5, 4)')
-
         stmt_1 = """
         SELECT * FROM t
         """
 
         try:
-            eng.execute(stmt_1)
+            pqueries.execute_query_in_parallel(self.eng, stmt_1, {})
         except:
             self.fail('Code failed on stmt_1')
 
-
         stmt_2 = """
-        SELECT * from t WHERE val1 < :param_1
+        SELECT * FROM t WHERE val1 < :param_1
         """
 
         try:
-            eng.execute(stmt_2, {'param_1': 4})
+            pqueries.execute_query_in_parallel(self.eng, stmt_2, {'param_1': 4})
         except:
             self.fail('Code failed on stmt_2')
+
+    def test_execute_query_in_parallel_run(self):
+
+        stmt = """
+        SELECT *
+        FROM t 
+        WHERE val1 IN (:param_1_PARALLEL) AND val2 < :param_2
+        """
+
+        try:
+            pqueries.execute_query_in_parallel(self.eng, stmt, {'param_1_PARALLEL': [1, 2, 5, 8], 'param_2': 8})
+
+        except:
+            self.fail('Code failed')
 
 
